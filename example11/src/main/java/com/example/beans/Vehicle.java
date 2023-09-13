019e8b7268ebe86d038d8cef7165714aa2a6da9b
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2023-09-13 19:05:54 +0100

    04 - Wiring Beans using @Autowiring

---
+++ example11/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,22 @@
+package com.example.beans;
+
+public class Vehicle {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("Printing Hello from component vehicle bean");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle name is - "+this.name;
+    }
+}
---
+++ example14/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package com.example.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name = "Toyota";
+
+    public Vehicle() {
+        System.out.println("Vehicle bean created by spring");
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("Printing Hello from component vehicle bean");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle name is - "+this.name;
+    }
+}
